8754bba51ef6a2e2444da4e040229a2bd6902971
Anton Abdulgalimov <aabdulgalimov@proton.me>
2023-10-12 17:26:40 +0200

    Fix c05_binary-search-tree-checker.py

---
+++ python/interview_cake/c05_2nd-largest-item-in-bst.py
@@ -0,0 +1,18 @@
+"""
+Write a method to find the 2nd largest element in a binary search tree.
+"""
+
+
+class BinaryTreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert_left(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insert_right(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
