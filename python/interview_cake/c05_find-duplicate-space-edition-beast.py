86e2dcaf12f93fded467486a312920c47113879b
Anton Abdulgalimov <aabdulgalimov@proton.me>
2023-10-25 21:51:11 +0200

    Add c05_find-duplicate-space-edition-beast.py

--- python/interview_cake/c05_find-duplicate-space-edition-beast.py
+++ python/interview_cake/c05_find-duplicate-space-edition-beast.py
@@ -7,28 +7,39 @@ It follows that our list has at least one integer which appears at least twice. 
 
 Write a function which finds an integer that appears more than once in our list. (If there are multiple duplicates, you only need to find one of them.)
 Optimize for space.
+Imagine each item in the list as a node in a linked list to get O(n) time.
 
-Time: O(n*lg n)
+Time: O(n)
 Space: O(1)
 """
 
 
-def find_duplicate(ints: list[int]) -> int:
-    start = 1
-    end = len(ints) - 1
-    while start < end:
-        middle = start + (end - start) // 2
-        lower_range_start, lower_range_end = start, middle
-        upper_range_start, upper_range_end = middle + 1, end
-        lower_count = 0
-        for i in ints:
-            if i >= lower_range_start and i <= lower_range_end:
-                lower_count += 1
-        if lower_count > (lower_range_end - lower_range_start + 1):
-            start, end = lower_range_start, lower_range_end
-        else:
-            start, end = upper_range_start, upper_range_end
-    return start
+def find_duplicate(ints):
+    n = len(ints) - 1
+
+    # get inside a cycle
+    position_in_cycle = n + 1
+    for _ in range(n):
+        position_in_cycle = ints[position_in_cycle - 1]
+
+    # find cycle length
+    start = position_in_cycle
+    p = ints[start - 1]
+    cycle_length = 1
+    while p != start:
+        p = ints[p - 1]
+        cycle_length += 1
+
+    # find first node of the cycle (duplicate value)
+    p1 = n + 1
+    p2 = p1
+    for _ in range(cycle_length):
+        p2 = ints[p2 - 1]
+    while p1 != p2:
+        p1 = ints[p1 - 1]
+        p2 = ints[p2 - 1]
+
+    return p1
 
 
 cases = (
@@ -37,7 +48,7 @@ cases = (
     ([1, 2, 3, 3], 3),
     ([1, 2, 3, 4, 5, 1], 1),
     ([1, 2, 3, 3, 3, 4, 5, 7], 3),
-    ([1, 1, 1, 1, 5, 5], 1),
+    ([1, 1, 1, 1, 5, 5], 5),
     ([4, 1, 4, 2, 3], 4),
 )
 for ints, want in cases:
