118026d71941811fcbdd2d86a8ee29a6fbd87f35
Anton Abdulgalimov <aabdulgalimov@proton.me>
2024-01-23 15:03:13 +0100

    Add c08_linked-list-cycle.py

---
+++ python/interview_cake/c08_linked-list-cycle.py
@@ -0,0 +1,52 @@
+"""
+Check whether a singly-linked list contains a cycle, given the first node of the list.
+
+Time: O(n)
+Space: O(1)
+"""
+
+
+class LinkedListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def contains_cycle(node: LinkedListNode) -> bool:
+    p1, p2 = node, node
+    while p1 is not None and p1.next is not None:
+        p1 = p1.next.next
+        p2 = p2.next
+        if p1 is p2:
+            return True
+    return False
+
+
+cases = (
+    (
+        1,
+        (
+            "n.next = LinkedListNode(2); n = n.next",
+            "n.next = LinkedListNode(3); n = n.next",
+            "n.next = LinkedListNode(4);",
+        ),
+        False,
+    ),
+    (
+        2,
+        (
+            "n.next = LinkedListNode(2); n = n.next",
+            "n.next = LinkedListNode(3); p = n; n = n.next",
+            "n.next = LinkedListNode(4); n = n.next",
+            "n.next = p",
+        ),
+        True,
+    ),
+)
+for case, ops, want in cases:
+    first_node = LinkedListNode(1)
+    n = first_node
+    for op in ops:
+        exec(op)
+    got = contains_cycle(first_node)
+    assert got == want, f"got: {got}, want: {want} ({case})"
